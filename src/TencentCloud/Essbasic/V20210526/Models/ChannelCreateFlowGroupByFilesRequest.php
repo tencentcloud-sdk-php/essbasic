974f31c18a79ee0fe4a0257a6f1a813671d50c82
tencentcloudapi <tencentcloudapi@tencent.com>
2022-09-12 08:28:01 +0800

    release 3.0.723

--- src/TencentCloud/Essbasic/V20210526/Models/ChannelCreateFlowGroupByFilesRequest.php
+++ src/TencentCloud/Essbasic/V20210526/Models/ChannelCreateFlowGroupByFilesRequest.php
@@ -18,31 +18,33 @@ namespace TencentCloud\Essbasic\V20210526\Models;
 use TencentCloud\Common\AbstractModel;
 
 /**
- * DescribeFlowDetailInfo请求参数结构体
+ * ChannelCreateFlowGroupByFiles请求参数结构体
  *
+ * @method array getFlowFileInfos() 获取每个子合同的发起所需的信息，数量限制2-100
+ * @method void setFlowFileInfos(array $FlowFileInfos) 设置每个子合同的发起所需的信息，数量限制2-100
+ * @method string getFlowGroupName() 获取合同组名称，长度不超过200个字符
+ * @method void setFlowGroupName(string $FlowGroupName) 设置合同组名称，长度不超过200个字符
  * @method Agent getAgent() 获取渠道应用相关信息。 此接口Agent.ProxyOrganizationOpenId、Agent. ProxyOperator.OpenId、Agent.AppId 和 Agent.ProxyAppId 均必填。
  * @method void setAgent(Agent $Agent) 设置渠道应用相关信息。 此接口Agent.ProxyOrganizationOpenId、Agent. ProxyOperator.OpenId、Agent.AppId 和 Agent.ProxyAppId 均必填。
- * @method array getFlowIds() 获取合同(流程)编号数组，最多支持100个。
-（备注：该参数和合同组编号必须二选一）
- * @method void setFlowIds(array $FlowIds) 设置合同(流程)编号数组，最多支持100个。
-（备注：该参数和合同组编号必须二选一）
  * @method UserInfo getOperator() 获取操作者的信息
  * @method void setOperator(UserInfo $Operator) 设置操作者的信息
- * @method string getFlowGroupId() 获取合同组编号（备注：该参数和合同(流程)编号数组必须二选一）
- * @method void setFlowGroupId(string $FlowGroupId) 设置合同组编号（备注：该参数和合同(流程)编号数组必须二选一）
  */
-class DescribeFlowDetailInfoRequest extends AbstractModel
+class ChannelCreateFlowGroupByFilesRequest extends AbstractModel
 {
+    /**
+     * @var array 每个子合同的发起所需的信息，数量限制2-100
+     */
+    public $FlowFileInfos;
+
+    /**
+     * @var string 合同组名称，长度不超过200个字符
+     */
+    public $FlowGroupName;
+
     /**
      * @var Agent 渠道应用相关信息。 此接口Agent.ProxyOrganizationOpenId、Agent. ProxyOperator.OpenId、Agent.AppId 和 Agent.ProxyAppId 均必填。
      */
     public $Agent;
-
-    /**
-     * @var array 合同(流程)编号数组，最多支持100个。
-（备注：该参数和合同组编号必须二选一）
-     */
-    public $FlowIds;
 
     /**
      * @var UserInfo 操作者的信息
@@ -50,16 +52,10 @@ class DescribeFlowDetailInfoRequest extends AbstractModel
     public $Operator;
 
     /**
-     * @var string 合同组编号（备注：该参数和合同(流程)编号数组必须二选一）
-     */
-    public $FlowGroupId;
-
-    /**
+     * @param array $FlowFileInfos 每个子合同的发起所需的信息，数量限制2-100
+     * @param string $FlowGroupName 合同组名称，长度不超过200个字符
      * @param Agent $Agent 渠道应用相关信息。 此接口Agent.ProxyOrganizationOpenId、Agent. ProxyOperator.OpenId、Agent.AppId 和 Agent.ProxyAppId 均必填。
-     * @param array $FlowIds 合同(流程)编号数组，最多支持100个。
-（备注：该参数和合同组编号必须二选一）
      * @param UserInfo $Operator 操作者的信息
-     * @param string $FlowGroupId 合同组编号（备注：该参数和合同(流程)编号数组必须二选一）
      */
     function __construct()
     {
@@ -74,22 +70,27 @@ class DescribeFlowDetailInfoRequest extends AbstractModel
         if ($param === null) {
             return;
         }
+        if (array_key_exists("FlowFileInfos",$param) and $param["FlowFileInfos"] !== null) {
+            $this->FlowFileInfos = [];
+            foreach ($param["FlowFileInfos"] as $key => $value){
+                $obj = new FlowFileInfo();
+                $obj->deserialize($value);
+                array_push($this->FlowFileInfos, $obj);
+            }
+        }
+
+        if (array_key_exists("FlowGroupName",$param) and $param["FlowGroupName"] !== null) {
+            $this->FlowGroupName = $param["FlowGroupName"];
+        }
+
         if (array_key_exists("Agent",$param) and $param["Agent"] !== null) {
             $this->Agent = new Agent();
             $this->Agent->deserialize($param["Agent"]);
         }
 
-        if (array_key_exists("FlowIds",$param) and $param["FlowIds"] !== null) {
-            $this->FlowIds = $param["FlowIds"];
-        }
-
         if (array_key_exists("Operator",$param) and $param["Operator"] !== null) {
             $this->Operator = new UserInfo();
             $this->Operator->deserialize($param["Operator"]);
-        }
-
-        if (array_key_exists("FlowGroupId",$param) and $param["FlowGroupId"] !== null) {
-            $this->FlowGroupId = $param["FlowGroupId"];
         }
     }
 }
